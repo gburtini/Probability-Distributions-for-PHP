46d8737424b40130787e4aac53163e6b3dd56180
Giuseppe Burtini <giuseppeburtini@gmail.com>
2015-10-25 18:32:47 -0700

    Fixing tabs.

--- src/Distributions/Normal.php
+++ src/Distributions/Normal.php
@@ -11,7 +11,6 @@
 	namespace gburtini\Distributions;
 	use gburtini\Distributions\Distribution;
 
-
 	class Normal extends Distribution {
 		// TODO: implement a skewness, kurtosis normal.
 		protected $mean;
@@ -36,8 +35,8 @@
 			return static::boxMuller()*sqrt($variance) + $mean;
 		}
 		public function cdf($x) {
-	        $d = $x - $this->mean;
-	        return 0.5 * (1 + $this->erf($d / (sqrt($this->variance) * sqrt(2))));2
+		        $d = $x - $this->mean;
+		        return 0.5 * (1 + $this->erf($d / (sqrt($this->variance) * sqrt(2))));2
 		}
 		public function icdf($y) {
 			// Inverse ncdf approximation by Peter John Acklam, implementation adapted to
@@ -119,37 +118,37 @@
 			return sqrt(-2 * log($u)) * cos(2 * M_PI * $v);
 		}
 
-	    public static function validateParameters($m, $v, $s, $k) {
-	        if(!is_numeric($m) || !is_numeric($v) || !is_numeric($s) || !is_numeric($k))
-	        	throw new \InvalidArgumentException("Non-numeric parameter in normal distribution (" . static::renderParameters(compact($m,$v,$s,$k)) . ").");
-
-	        if($v < 0)
-	        	throw new \InvalidArgumentException("Variance must be strictly positive (it is σ-squared after all!). Currently, \$v = " . var_export($v) . ".");
-
-	    }
-
-	    protected static function erf($x) {
-	    	// translation of Press, Teukolsky, Vetterling, Flannery (2001) approximation.
-	    	// https://en.wikipedia.org/wiki/Error_function#Numerical_approximation 
-	        $t = 1 / (1 + 0.5 * abs($x));
-	        $tau = $t * exp(
-	                - $x * $x
-	                - 1.26551223
-	                + 1.00002368 * $t
-	                + 0.37409196 * pow($t, 2)
-	                + 0.09678418 * pow($t, 3)
-	                - 0.18628806 * pow($t, 4)
-	                + 0.27886807 * pow($t, 5)
-	                - 1.13520398 * pow($t, 6)
-	                + 1.48851587 * pow($t, 7)
-	                - 0.82215223 * pow($t, 8)
-	                + 0.17087277 * pow($t, 9));
-
-	        if ($x >= 0) {
-	            return 1 - $tau;
-	        } else {
-	            return $tau - 1;
-	        }
-	    }
+		public static function validateParameters($m, $v, $s, $k) {
+			if(!is_numeric($m) || !is_numeric($v) || !is_numeric($s) || !is_numeric($k))
+				throw new \InvalidArgumentException("Non-numeric parameter in normal distribution (" . static::renderParameters(compact($m,$v,$s,$k)) . ").");
+			
+			if($v < 0)
+				throw new \InvalidArgumentException("Variance must be strictly positive (it is σ-squared after all!). Currently, \$v = " . var_export($v) . ".");
+			
+		}
+		
+		protected static function erf($x) {
+			    // translation of Press, Teukolsky, Vetterling, Flannery (2001) approximation.
+			    // https://en.wikipedia.org/wiki/Error_function#Numerical_approximation 
+			$t = 1 / (1 + 0.5 * abs($x));
+			$tau = $t * exp(
+			        - $x * $x
+			        - 1.26551223
+			        + 1.00002368 * $t
+			        + 0.37409196 * pow($t, 2)
+			        + 0.09678418 * pow($t, 3)
+			        - 0.18628806 * pow($t, 4)
+			        + 0.27886807 * pow($t, 5)
+			        - 1.13520398 * pow($t, 6)
+			        + 1.48851587 * pow($t, 7)
+			        - 0.82215223 * pow($t, 8)
+			        + 0.17087277 * pow($t, 9));
+			
+			if ($x >= 0) {
+			    return 1 - $tau;
+			} else {
+			    return $tau - 1;
+			}
+		}
 	}
 ?>
