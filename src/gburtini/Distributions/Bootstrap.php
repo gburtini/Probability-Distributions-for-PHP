8e209edeeaf2a293153c7823d879df4e1f6358bc
Giuseppe Burtini <joe@truephp.com>
2015-11-05 11:18:17 -0800

    Fixing namespace for SplFixedArray. Might occur elsewhere.

--- src/gburtini/Distributions/Bootstrap.php
+++ src/gburtini/Distributions/Bootstrap.php
@@ -5,7 +5,7 @@
     // TODO: this file is completely unfinished: consider implementing this as a Distribution itself with all the associated methods.
     
 		public static function resampleWithReplacement($data, $n) {
-			$newData = new SplFixedArray($n);
+			$newData = new \SplFixedArray($n);
 			$originalLength = count($data);
 			for($i = 0; $i < $n; $i++) { 
 				$k = rand(0, $originalLength);
